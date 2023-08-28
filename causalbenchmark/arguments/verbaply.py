1e650a04abbeb0495ff02696b310eff144251c9f
felixludos <felix.leeb@hotmail.com>
2023-08-28 11:27:07 +0200

    minor updates

--- causalbenchmark/arguments/verbaply.py
+++ causalbenchmark/arguments/verbaply.py
@@ -94,6 +94,21 @@ class SimpleTemplater:
 		# 						   or (i in self.reqs and self.reqs[i] in reqs))
 
 
+class FixedTemplate(AbstractTool, SimpleTemplater):
+	def __init__(self, gizmo: str, template: str, **kwargs):
+		super().__init__(template, **kwargs)
+		self.gizmo = gizmo
+
+	def gizmos(self) -> Iterator[str]:
+		yield self.gizmo
+
+	def grab_from(self, ctx: Optional['AbstractContext'], gizmo: str) -> Any:
+		assert gizmo == self.gizmo
+		reqs = {key: ctx.grab_from(ctx, key) for key in self.keys}
+		out = self.fill_in(reqs)
+		return out
+
+
 class TemplateDecision(Decision):
 	_Templater = SimpleTemplater
 	def __init__(self, *args, **kwargs):
@@ -301,7 +316,8 @@ def default_vocabulary(seed=None):
 
 	verb.include(Decision('precise_text', full['precise']['options']))
 
-	verb.include(AsSentence('claim', capitalize=True, period=True))
+	verb.include(FixedTemplate('sentence', '{claim}'))
+	verb.include(AsSentence('sentence', capitalize=True, period=True))
 	return verb
 
 
@@ -362,10 +378,10 @@ def test_spawn_templates():
 		# 'verb': 'eat dinner',
 	}))
 
-	entries = list(gen.spawn('claim'))
+	entries = list(gen.spawn('sentence'))
 	ctx = gen.current
 
-	results = [entry['claim'] for entry in entries]
+	results = [entry['sentence'] for entry in entries]
 
 	count = sum(r is not None for r in results)
 	fraction = count / len(results)

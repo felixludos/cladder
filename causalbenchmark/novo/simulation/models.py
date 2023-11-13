c1ec864d42755f01c005c17a7df194780e0dc428
felixludos <felixludos.info@gmail.com>
2023-11-13 17:54:46 +0100

    prep test cf

--- causalbenchmark/novo/simulation/models.py
+++ causalbenchmark/novo/simulation/models.py
@@ -1,5 +1,5 @@
 
-
+from omnibelt import toposort
 import omnifig as fig
 
 import numpy as np
@@ -19,10 +19,10 @@ def get_rng(seed, reset_master=True):
 
 
 class Variable(fig.Configurable):
-	def __init__(self, *, rng=None, varname=None, **kwargs):
+	def __init__(self, *, rng=None, name=None, **kwargs):
 		super().__init__(**kwargs)
 		self._rng = rng
-		self._varname = varname
+		self._name = name
 
 
 	@staticmethod
@@ -32,7 +32,7 @@ class Variable(fig.Configurable):
 			logits = torch.randn(shape) if logits is None else torch.tensor(logits).float()
 			probs = logits.softmax(dim=-1)
 		else:
-			probs = torch.tensor(probs).float()
+			probs = torch.tensor(probs).float() if isinstance(probs, (list, tuple)) else probs.clone().float()
 			assert probs.min() >= 0
 			probs = probs / probs.sum(dim=-1, keepdim=True)
 		return probs
@@ -40,10 +40,10 @@ class Variable(fig.Configurable):
 
 	@property
 	def name(self) -> str:
-		return self._varname
+		return self._name
 	@name.setter
 	def name(self, value):
-		self._varname = value
+		self._name = value
 
 
 	@property
@@ -57,24 +57,53 @@ class Variable(fig.Configurable):
 
 
 	def implied_edges(self):
-		return [(p, self) for p in self.parents]
-
-
-	def __str__(self):
-		name = getattr(self, 'name')
-		if name is None:
-			name = self.__class__.__name__
-		return f'{name}({self.n})'
+		return [(parent.name, self.name) for parent in self.parents]
 
 
 	def __repr__(self):
-		return super().__str__()
+		name = getattr(self, 'name')
+		if name is None:
+			name = self.__class__.__name__
+
+		suffix = ''
+		if len(self.parents):
+			suffix = f' | {", ".join(p.name for p in self.parents)}'
+
+		return f'{name}(n={self.n}{suffix})'
 
 
 	def as_hard_intervention(self, value: int):
 		probs = torch.zeros(self.n)
 		probs[value] = 1
 		return Prior(probs=probs, name=self.name)
+
+
+
+class BernoulliVariable(Variable):
+	def __repr__(self):
+		name = getattr(self, 'name')
+		if name is None:
+			name = self.__class__.__name__
+
+		suffix = ''
+		if len(self.parents):
+			suffix = f' | {", ".join(p.name for p in self.parents)}'
+
+		return f'p({name}{suffix})'
+
+
+	@property
+	def param(self):
+		return self.probs[0][...,1]
+
+
+	def as_intervention(self, value: float):
+		assert 0 <= value <= 1, f'Value must be in [0, 1], got {value}'
+		return Bernoulli(prob=value, name=self.name)
+
+
+	def as_hard_intervention(self, value: int):
+		return self.as_intervention(value=value)
 
 
 
@@ -92,7 +121,7 @@ class Prior(Variable, _CategoricalBase):
 
 
 @fig.component('bern')
-class Bernoulli(Prior):
+class Bernoulli(Prior, BernoulliVariable):
 	def __init__(self, prob=None, *, logit=None, probs=None, logits=None, **kwargs):
 		if prob is not None:
 			probs = [1-prob, prob]
@@ -106,19 +135,26 @@ class Bernoulli(Prior):
 		return self.probs[0][1]
 
 
-	def __str__(self):
+	def __repr__(self):
 		name = getattr(self, 'name')
 		if name is None:
 			name = self.__class__.__name__
-		return f'{name}({self.p:.2f})'
+
+		# suffix = ''
+		if len(self.parents):
+			suffix = f' | {", ".join(p.name for p in self.parents)}'
+		else:
+			suffix = f'={self.p:.2f}'.rstrip('0').rstrip('.')
+		return f'p({name}{suffix})'
 
 
 
 @fig.component('concat')
 class Conditional(Variable, _ConditionalCategoricalBase):
 	def __init__(self, parents, probs=None, *, logits=None, n=2, **kwargs):
+		# IMPORTANT: don't rely on parents for sampling or inference, only for structure
 		probs = self.process_raw_params(probs, logits=logits, n=n, parentshapes=tuple(p.n for p in parents))
-		super().__init__(probs=probs.tolist(), **kwargs)
+		super().__init__(probs=[probs.tolist()], **kwargs)
 		self._parents = tuple(parents)
 
 
@@ -134,7 +170,7 @@ class Conditional(Variable, _ConditionalCategoricalBase):
 
 
 @fig.component('conbern')
-class ConditionalBernoulli(Conditional):
+class ConditionalBernoulli(Conditional, BernoulliVariable):
 	def __init__(self, parents, conds=None, *, logit_conds=None, probs=None, logits=None, **kwargs):
 		if conds is not None:
 			probs = torch.tensor(conds).float()
@@ -154,18 +190,16 @@ class Network(fig.Configurable, _BayesianNetworkBase):
 		for name, v in variables.items():
 			v.name = name
 		nodes = [v for v in variables.values()]
-		edges = [e for v in variables.values() for e in v.implied_edges()]
+		edges = [(variables[s], variables[e]) for v in variables.values() for s, e in v.implied_edges()]
 		super().__init__(distributions=nodes, edges=edges, **kwargs)
 		self._variables = variables
 		self._rng = rng
-
-
-	def __str__(self):
-		return f'{self.__class__.__name__}({", ".join(self._variables.keys())})'
+		order = toposort({v.name: [p.name for p in v.parents] for v in self._variables.values()})
+		self.vars = [self._variables[name] for name in order]
 
 
 	def __repr__(self):
-		return super().__str__()
+		return f'{self.__class__.__name__}({", ".join(v.name for v in self.vars)})'
 
 
 	def __getitem__(self, item):
@@ -176,15 +210,68 @@ class Network(fig.Configurable, _BayesianNetworkBase):
 		return len(self._variables)
 
 
-
-	def marginals(self, **conds):
-		pass
-
-
-	# def interventions(self, **conds):
-	# 	pass
+	def parents_of(self, var: Variable | str):
+		if isinstance(var, str):
+			var = self._variables[var]
+		return [self._variables[parent.name] for parent in var.parents]
 
 
+	def marginals(self, **conds: int):
+		mask = []
+		vals = []
+
+		conds = {k: torch.tensor(v).long() for k, v in conds.items()}
+		size = next(iter(conds.values())).size() if len(conds) else ()
+		default_size = len(size) == 0
+		neutral = torch.zeros(size).long()
+
+		for var in self.vars:
+			mask.append(var.name in conds)
+			vals.append(conds.get(var.name, neutral))
+
+		mask = torch.tensor(mask).view(-1, len(self.vars))
+		vals = torch.stack(vals, dim=-1).view(-1, len(self.vars))
+		X = torch.masked.MaskedTensor(vals, mask)
+
+		mars = self.predict_proba(X)
+		return {var.name: mar.squeeze(0) if default_size else mar for var, mar in zip(self.vars, mars)}
+
+
+	def intervene(self, **interventions: int):
+		newvars = [var.as_hard_intervention(interventions[var.name]) if var.name in interventions else var
+				   for var in self.vars]
+		return self.__class__(variables=newvars, rng=self._rng)
+
+
+	def ate(self, treatment: str, *, treated_val=1, not_treated_val=0):
+		treated = self.intervene(**{treatment: treated_val}).marginals()
+		not_treated = self.intervene(**{treatment: not_treated_val}).marginals()
+		return {name: treated[name] - not_treated[name] for name in self.vars}
+
+
+
+@fig.component('bernet')
+class BernoulliNetwork(Network):
+	vars: list[BernoulliVariable]
+
+	def __init__(self, variables: list[BernoulliVariable] | dict[str, BernoulliVariable], **kwargs):
+		super().__init__(variables, **kwargs)
+		assert all(isinstance(v, BernoulliVariable) for v in self._variables.values())
+
+
+	def __repr__(self):
+		return ' '.join(str(v) for v in reversed(self.vars))
+
+
+	def marginals(self, **conds: int):
+		mars = super().marginals(**conds)
+		return {name: mar[..., 1] for name, mar in mars.items()}
+
+
+	def intervene(self, **interventions: float):
+		newvars = [var.as_intervention(interventions[var.name]) if var.name in interventions else var
+				   for var in self.vars]
+		return self.__class__(variables=newvars, rng=self._rng)
 
 
 
--- causalbenchmark/novo/simulation/unit_tests.py
+++ causalbenchmark/novo/simulation/unit_tests.py
@@ -1,6 +1,6 @@
 from ..imports import *
 
-from .models import Prior, Conditional, Network, Bernoulli, ConditionalBernoulli
+from .models import Prior, Conditional, Network, Bernoulli, ConditionalBernoulli, BernoulliNetwork
 
 
 	
@@ -11,23 +11,23 @@ def test_vars():
 	mu = b.sample(1000).view(-1).float().mean().item()
 	assert abs(mu - 0.5) < 0.05
 
-	p = Bernoulli(.6, varname='p')
-	c = ConditionalBernoulli([p], [[.5, .5]], varname='c')
+	X = Bernoulli(.6, name='X')
+	Y = ConditionalBernoulli([X], [.5, .5], name='Y')
 
-	xs = p.sample(1000)
+	xs = X.sample(1000)
 	mu1 = xs.float().mean().item()
 	assert abs(mu1 - 0.6) < 0.05
-	ys = c.sample(1000, xs.unsqueeze(1))
+	ys = Y.sample(1000, xs.unsqueeze(1))
 	mu2 = ys.float().mean().item()
 	assert abs(mu2 - 0.5) < 0.05
 
 
 
 def test_net():
-	p = Bernoulli(.6, varname='p')
-	c = ConditionalBernoulli([p], [[.5, .5]], varname='c')
+	X = Bernoulli(.6, name='X')
+	Y = ConditionalBernoulli([X], [.5, .5], name='Y')
 
-	net = Network([p, c])
+	net = Network([X, Y])
 
 	xs = net.sample(1000).float().mean(0)
 
@@ -35,12 +35,72 @@ def test_net():
 
 
 
+def test_marginals():
+	X = Bernoulli(.6, name='X')
+	Y = ConditionalBernoulli([X], [.3, .8], name='Y')
+
+	net = BernoulliNetwork([X, Y])
+
+	marginals = net.marginals()
+
+	assert (torch.allclose(marginals['X'], torch.tensor(0.6))
+			and torch.allclose(marginals['Y'], torch.tensor(0.6)))
+
+	marginals = net.marginals(X=1)
+
+	assert (torch.allclose(marginals['X'], torch.tensor(1.0))
+			and torch.allclose(marginals['Y'], torch.tensor(0.8)))
+
+	marginals = net.marginals(Y=1)
+
+	assert (torch.allclose(marginals['X'], torch.tensor(0.8))
+			and torch.allclose(marginals['Y'], torch.tensor(1.0)))
 
 
 
+def test_interventions():
+	X = Bernoulli(.6, name='X')
+	Y = ConditionalBernoulli([X], [[.3, .8]], name='Y')
+
+	net = BernoulliNetwork([X, Y])
+
+	intervened = net.intervene(X=1)
+	marginals = intervened.marginals()
+
+	assert (torch.allclose(marginals['X'], torch.tensor(1.0))
+			and torch.allclose(marginals['Y'], torch.tensor(0.8)))
+
+	intervened = net.intervene(Y=1)
+	marginals = intervened.marginals()
+
+	assert (torch.allclose(marginals['X'], torch.tensor(0.6))
+			and torch.allclose(marginals['Y'], torch.tensor(1.0)))
 
 
 
+def test_categorical():
+
+	X = Prior([0.1, 0.6, 0.3], name='X')
+
+	yvals = torch.tensor([.3, .1, .8])
+	Y = Conditional([X], torch.stack([1-yvals, yvals],-1), name='Y')
+
+	net = Network([X, Y])
+
+	marginals = net.marginals()
+
+	assert (torch.allclose(marginals['X'], torch.tensor([0.1, 0.6, 0.3]))
+			and torch.allclose(marginals['Y'], torch.tensor([0.67, 0.33])))
+
+
+def test_confounding():
+
+	U = Bernoulli(0.1)
+	X = ConditionalBernoulli([U], [0.4, 0.6])
+	Y = ConditionalBernoulli([U, X], [[0.3, 0.7], [0.8, 0.2]])
+	net = BernoulliNetwork({'U': U, 'X': X, 'Y': Y})
+
+	print(net)
 
 
 

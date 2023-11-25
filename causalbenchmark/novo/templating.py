3f5cae6cd6032066ec507bb9df31d64f0ca54d81
felixludos <felixludos.info@gmail.com>
2023-11-25 18:11:34 +0100

    added another story

--- causalbenchmark/novo/templating.py
+++ causalbenchmark/novo/templating.py
@@ -1,36 +1,20 @@
 from .imports import *
-from omnibelt import pformat
+from omnibelt import pformat, pformat_vars
 
 
 class SimpleTemplater:
 	def __init__(self, template: str, **kwargs):
 		super().__init__(**kwargs)
-		keys = set(self._extract_template_keys(template))
+		keys = list(pformat_vars(template))
 		self.keys = keys
 		self.template = template
 
 
-	@staticmethod
-	def _extract_template_keys(template: str):
-		# pattern = r'\{([_a-zA-Z][_a-zA-Z0-9]*)\}'
-		# pattern = r'(?<!\{)\{([_a-zA-Z][_a-zA-Z0-9]*)\}(?!\})'
-		# pattern = r'(?<!\{)\{([_a-zA-Z][_a-zA-Z0-9]*(?:!s|!r|!a)?(?:\:[^}]*)?)\}(?!\})'
-
-		pattern = r'\{([_a-zA-Z][_a-zA-Z0-9]*(?:!s|!r|!a)?(?:\:[^}]*)?)\}'
-		for match in re.finditer(pattern, template):
-			if '{' not in match.group(1) and '}' not in match.group(1):
-				start_pos, end_pos = match.start(), match.end()
-				if (start_pos == 0 or template[start_pos - 1] != '{') \
-					and (end_pos == len(template) or template[end_pos] != '}'):
-					yield match.group(1).split('!')[0].split(':')[0]
-
-
-
-	def fill_in(self, reqs: Dict[str, str] = {}, **vals: str):
+	def fill_in(self, reqs: dict[str, str] = {}, **vals: str):
 		# use pformat?
 		vals.update({key: reqs[key] for key in self.keys if key not in vals})
 		# return self.template.format(**vals)
-		return pformat(self.template, **vals)
+		return pformat(self.template, reqs, **vals)
 
 
 
@@ -44,11 +28,13 @@ class FixedTemplate(AbstractGadget, SimpleTemplater):
 		yield self.gizmo
 
 
-	def grab_from(self, ctx: Optional[AbstractGig], gizmo: str) -> Any:
-		assert gizmo == self.gizmo
+	def grab_from(self, ctx: Optional[AbstractGig], gizmo: str = None) -> Any:
+		# assert gizmo == self.gizmo
+		# reqs = {key: ctx.grab_from(ctx, key) for key in self.keys}
+		# out = self.fill_in(reqs)
+		# return out
 		reqs = {key: ctx.grab_from(ctx, key) for key in self.keys}
-		out = self.fill_in(reqs)
-		return out
+		return self.fill_in(reqs)
 
 
 
